15acfe0684257fcfb5c5063c5a5ca6c00dc803d4
Yunus Emre TURHAN <emreturhanofficial34@gmail.com>
2024-09-12 00:48:34 +0300

    2.gonderim

---
+++ 26.Haftanıun_Günleri_Uygulaması/app.js
@@ -0,0 +1,34 @@
+// let sayi = prompt("Lütfen 1 ile beş arasında bir sayı giriniz:");
+
+// switch (sayi){
+//     case "1":
+//         console.log("Girilen sayı 1'dir.");
+//         break;
+//     case "2":
+//         console.log("Girilen sayı 2'dir.");
+//         break;
+//     case "3":
+//         console.log("Girilen sayı 3'tür.");
+//         break;
+//     case "4":
+//         console.log("Girilen sayi 4'tür.");
+//         break;
+//     case "5":
+//         console.log("Girilen sayı 5'tir");
+//         break;
+//     default:
+//         console.log("Girilen sayı 1 ile 5 arasında olmalıdır.");
+//         break;
+// }
+
+let satir ="\r\n";
+let metin="1-Pazartesi"+satir
++"2-Salı"+satir
++"3-Çarşamba"+satir
++"4-Perşembe"+satir
++"5-Cuma"+satir
++"6-Cumartesi"+satir
++"7-Pazar"+satir
++"Günlerden birini seçiniz";
+
+let deger = prompt(metin);
