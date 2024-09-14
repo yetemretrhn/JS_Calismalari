58e19012c18572f110e8fa8de264e1c73f2e42b7
Yunus Emre TURHAN <emreturhanofficial34@gmail.com>
2024-09-14 20:09:21 +0300

    son hali

--- 26.Haftanıun_Günleri_Uygulaması/app.js
+++ 26.Haftanıun_Günleri_Uygulaması/app.js
@@ -31,4 +31,31 @@ let metin="1-Pazartesi"+satir
 +"7-Pazar"+satir
 +"Günlerden birini seçiniz";
 
-let deger = prompt(metin);
+let deger =prompt(metin);
+switch(deger){
+    case"1":
+        console.log("Pazartesi");
+        break;
+    case "2":
+        console.log("Salı");
+        break;
+    case "3":
+        console.log("Çarşamba");
+        break;
+    case "4":
+        console.log("perşembe");
+        break;
+    case "5":
+        console.log("cuma");
+        break;
+    case "6":
+        console.log("cumartesi");
+        break;
+    case "7":
+        console.log("pazar");
+        break;
+    default:
+        console.log("Girilen sayı 1 ile 5 arasında olmalıdır!");
+        break;
+}
+
